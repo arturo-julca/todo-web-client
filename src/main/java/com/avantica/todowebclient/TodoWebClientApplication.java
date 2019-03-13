b65a749dd37310b5cbc1573d059f626c630f418a
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-13 12:55:47 -0500

    First commit

---
+++ src/main/java/com/avantica/todowebclient/TodoWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todowebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebClientApplication.class, args);
+	}
+
+}
