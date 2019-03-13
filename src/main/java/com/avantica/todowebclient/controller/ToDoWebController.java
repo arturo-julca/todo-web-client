286728a38bfb21663bb9db5a1e5541a41192a208
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-13 17:03:41 -0500

    Added support to rest call reminder service directly

--- src/main/java/com/avantica/todowebclient/controller/ToDoWebController.java
+++ src/main/java/com/avantica/todowebclient/controller/ToDoWebController.java
@@ -1,21 +1,13 @@
 package com.avantica.todowebclient.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.avantica.todowebclient.model.ToDo;
@@ -30,13 +22,7 @@ public class ToDoWebController {
 	@RequestMapping(value = "/todo", method = RequestMethod.GET)
     public ModelAndView listToDos() {
         ModelAndView modelAndView = new ModelAndView("todo_list");
-
         List<ToDo> listToDos = todoRestClient.getAllToDos();
-//        	List<ToDo> listToDos = new ArrayList<ToDo>();
-//        listToDos.add(new ToDo(1L, "first todo", LocalDateTime.now(), "test1@email.com"));
-//        listToDos.add(new ToDo(2L, "second todo", LocalDateTime.now(), "test2@email.com"));
-//        listToDos.add(new ToDo(3L, "third todo", LocalDateTime.now(), "test3@email.com"));
-        
         modelAndView.addObject("listToDos", listToDos);
         return modelAndView;
     }
@@ -48,12 +34,6 @@ public class ToDoWebController {
         return modelAndView;
     }
 
-	//public String addTodo(@Valid ToDo toDo, BindingResult result, Model model) {
-//	@RequestMapping(value = "/todo/add", method = RequestMethod.POST)
-//	public String Todo(ToDo toDo) {
-//        System.out.println(toDo);
-//		return "redirect:/todo";
-//    }
 	@RequestMapping(value = "/todo/add", method = RequestMethod.POST)
 	public ModelAndView Todo(ToDo toDo) {
 		todoRestClient.addTodo(toDo);
@@ -62,40 +42,17 @@ public class ToDoWebController {
 	
 	@RequestMapping(path = "/todo/edit/{id}", method = RequestMethod.GET)
     public ModelAndView editProduct(Model model, @PathVariable(value = "id") String id) {
-        //model.addAttribute("product", productRepository.findOne(id));
+		ToDo toDo = todoRestClient.getToDo(Long.parseLong(id));
 		ModelAndView modelAndView = new ModelAndView("todo_edit");		
-        modelAndView.addObject("toDo", new ToDo(1L, "first todo", LocalDateTime.now(), "test1@email.com"));
+        modelAndView.addObject("toDo", toDo);
         return modelAndView;
     }
 
     @RequestMapping(path = "/todo/delete/{id}", method = RequestMethod.GET)
     public ModelAndView deleteProduct(@PathVariable(name = "id") String id) {
-        //productRepository.delete(id);
+    	todoRestClient.deleteTodo(Long.parseLong(id));
         return new ModelAndView("redirect:/todo");
     }
     
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView listToDos2() {
-    	RestTemplate restTemplate = new RestTemplate();
-    	String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjIiLCJhdXRob3JpdGllcyI6WyJST0xFX0FETUlOIl0sImlhdCI6MTU1MjUwMTY5NywiZXhwIjoxNTUyNTg4MDk3fQ.UwD7RZ1QrsqbxrlZ6pyglr9ABPVelnbZ22U1waqeGCQXl6XDAFheBhzMiURYhgNlkThx0b_Zg6bhJFT179Pnwg";
-    	System.out.println("=========test===========");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", "Bearer " + token);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<List<ToDo>> response = restTemplate.exchange(
-                "http://localhost:8762/reminder/todo",
-                HttpMethod.GET, 
-                entity, 
-                new ParameterizedTypeReference<List<ToDo>>() {});
-
-        
-        
-        ModelAndView modelAndView = new ModelAndView("todo_list");
-
-        List<ToDo> listToDos = response.getBody();
-        modelAndView.addObject("listToDos", listToDos);
-        return modelAndView;
-    }
 }
--- src/main/java/com/avantica/todowebclient/rest/TodoRestClient.java
+++ src/main/java/com/avantica/todowebclient/rest/TodoRestClient.java
@@ -2,8 +2,14 @@ package com.avantica.todowebclient.rest;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -13,10 +19,16 @@ import com.avantica.todowebclient.model.ToDo;
 @Repository
 public class TodoRestClient {
 
+	@Autowired
+	private Environment env;
+	
+	@Value("${todo.server.url}")
+	private String SERVICE_URL;
+	
 	public List<ToDo> getAllToDos() {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<List<ToDo>> response = restTemplate.exchange(
-				"http://localhost:9090/todo/",
+				SERVICE_URL,
 				HttpMethod.GET, 
 				null, 
 				new ParameterizedTypeReference<List<ToDo>>() {});
@@ -24,11 +36,37 @@ public class TodoRestClient {
 		return listToDos;
 	}
 	
+	public ToDo getToDo(long id) {
+		RestTemplate restTemplate = new RestTemplate();
+		ToDo toDo = restTemplate.getForObject(
+				SERVICE_URL + "/" + id, 
+				ToDo.class);		
+		return toDo;
+	}
+	
 	public void addTodo(ToDo toDo){
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.postForObject(
-				"http://localhost:9090/todo/", 
+				SERVICE_URL, 
 				toDo, 
 				ToDo.class);
+	}
+	
+	public void updateTodo(ToDo toDo){
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		HttpEntity<ToDo> requestUpdate = new HttpEntity<>(toDo, headers);
+		restTemplate.exchange(
+				SERVICE_URL,
+				HttpMethod.PUT,
+				requestUpdate, 
+				ToDo.class);
+	}
+	
+	public void deleteTodo(Long id){
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.delete(
+				SERVICE_URL + "/" + id);
 	}
 }
