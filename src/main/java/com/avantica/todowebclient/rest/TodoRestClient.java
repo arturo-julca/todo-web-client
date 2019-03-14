ba6005acd5d2c9f47f1b43cbd3d6876217915456
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-13 19:12:09 -0500

    Handling bearer tokens on rest calls

--- src/main/java/com/avantica/todowebclient/rest/TodoRestClient.java
+++ src/main/java/com/avantica/todowebclient/rest/TodoRestClient.java
@@ -2,10 +2,8 @@ package com.avantica.todowebclient.rest;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -18,19 +16,19 @@ import com.avantica.todowebclient.model.ToDo;
 
 @Repository
 public class TodoRestClient {
-
-	@Autowired
-	private Environment env;
 	
-	@Value("${todo.server.url}")
+	@Value("${reminder.server.url}")
 	private String SERVICE_URL;
+	
+	@Value("${auth.bearer.token}")
+	private String TOKEN;
 	
 	public List<ToDo> getAllToDos() {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<List<ToDo>> response = restTemplate.exchange(
 				SERVICE_URL,
 				HttpMethod.GET, 
-				null, 
+				getHttpEntity(null), 
 				new ParameterizedTypeReference<List<ToDo>>() {});
 		List<ToDo> listToDos = response.getBody();
 		return listToDos;
@@ -38,35 +36,52 @@ public class TodoRestClient {
 	
 	public ToDo getToDo(long id) {
 		RestTemplate restTemplate = new RestTemplate();
-		ToDo toDo = restTemplate.getForObject(
-				SERVICE_URL + "/" + id, 
-				ToDo.class);		
-		return toDo;
+		ResponseEntity<ToDo> response = restTemplate.exchange(
+				SERVICE_URL + "/" + id,
+				HttpMethod.GET, 
+				getHttpEntity(null), 
+				new ParameterizedTypeReference<ToDo>() {});
+		ToDo todo = response.getBody();
+		return todo;
 	}
 	
 	public void addTodo(ToDo toDo){
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.postForObject(
-				SERVICE_URL, 
-				toDo, 
+		restTemplate.exchange(
+				SERVICE_URL,
+				HttpMethod.POST,
+				getHttpEntity(toDo), 
 				ToDo.class);
 	}
 	
 	public void updateTodo(ToDo toDo){
 		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		HttpEntity<ToDo> requestUpdate = new HttpEntity<>(toDo, headers);
 		restTemplate.exchange(
 				SERVICE_URL,
 				HttpMethod.PUT,
-				requestUpdate, 
+				getHttpEntity(toDo), 
 				ToDo.class);
 	}
 	
 	public void deleteTodo(Long id){
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.delete(
-				SERVICE_URL + "/" + id);
+		restTemplate.exchange(
+				SERVICE_URL + "/" + id,
+                HttpMethod.DELETE, 
+                getHttpEntity(null), 
+                new ParameterizedTypeReference<ToDo>() {});
 	}
+	
+	private HttpEntity<?> getHttpEntity(ToDo toDo) {
+    	HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Bearer " + TOKEN);
+        HttpEntity<?> entity;
+        if(toDo!=null){
+        	entity = new HttpEntity<>(toDo, headers);
+        }else{
+        	entity = new HttpEntity<>(headers);
+        }        
+        return entity;
+    }
 }
